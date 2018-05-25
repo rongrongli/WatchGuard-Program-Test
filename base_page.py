dafb6d61cfe36f585320a5fb88fb1a543face3a7
lirong <18349235916@163.com>
2018-05-25 10:33:08 +0800

    add:base_page.py

---
+++ base_page.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from selenium import webdriver
+
+class BasePage(object):
+    """
+    basic page for inherit
+    """
+
+    def __init__(self, selenium_driver):
+        self.driver = selenium_driver
